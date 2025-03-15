b4a28e93f9f60f50cf3432cc5418082cc80e1a4f
Fuadmikayil <mikayilov2006@gmail.com>
2025-03-15 10:12:04 +0400

    LoginPage

--- app/login/page.jsx
+++ app/login/page.jsx
@@ -1,8 +1,25 @@
+import Header from "@/Components/Header";
 
 export default function LoginPage() {
   return (
-    <div>
-      LoginPage
-    </div>
-  )
+    <>
+      <Header />
+      <div className="flex items-center justify-center py-[20px]">
+        <h1 className="text-3xl  font-bold tracking-wider">LoginPage </h1>
+      </div>
+      <div className="flex items-center justify-center py-[20px]">
+        <form action="">
+          <div>
+            <label htmlFor="username">Username</label>
+            <input name="username" id="username" type="text" />
+          </div>{" "}
+          <div>
+            <label htmlFor="password">Password</label>
+            <input name="password" id="password" type="text" />
+          </div>
+          <button>Login</button>
+        </form>
+      </div>
+    </>
+  );
 }
