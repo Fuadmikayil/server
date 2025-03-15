1487b3f37eab85f93811f2cd23bb4b5c972a24df
Fuadmikayil <mikayilov2006@gmail.com>
2025-03-15 10:07:06 +0400

    Header

--- app/page.jsx
+++ app/page.jsx
@@ -4,8 +4,8 @@ export default function HomePage() {
   return (
   <>
   <Header/>
-    <div>
-      <h1>HomePage </h1>
+    <div className="flex items-center justify-center py-[100px]">
+    <h1 className='text-3xl  font-bold tracking-wider'>HomePage </h1>
     </div>
   </>
   );
